aa2f59f1752316f95bfd8e66a0c0e1bffb44915f
rhacker <nxqd.inbox+rhacker@gmail.com>
2024-01-22 15:26:06 +0700

    dev: update solidity version

--- src/utils/SafeTransferLib.sol
+++ src/utils/SafeTransferLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 /// @notice ETH and ERC20 transfer lib that handles missing return values gracefully.
 /// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/SafeTransferLib.sol)
